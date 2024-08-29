9cac346b2d1e1636526eb010cd20e884d1b82128
Reece Moore <reecemoore@192.168.1.113>
2024-08-29 12:03:17 +0100

    Added the coffee lover controller

--- Northcoders/backend/drinksapi/src/main/java/com/northcoders/drinksapi/controller/HomeController.java
+++ Northcoders/backend/drinksapi/src/main/java/com/northcoders/drinksapi/controller/HomeController.java
@@ -12,4 +12,9 @@ public class HomeController {
     public String home(){
         return "Welcome to the Drinks API!";
     }
+
+    @GetMapping("/coffeelover")
+    public String coffee(){
+        return "I like coffee!";
+    }
 }
