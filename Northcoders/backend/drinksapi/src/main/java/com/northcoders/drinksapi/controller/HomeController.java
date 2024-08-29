06b809f132d9dd2068885a32f6c8f7269f0c1448
Jez <jezdavisuk@gmail.com>
2024-08-29 12:36:12 +0100

    add: GET request to /coffee URI: JSON Format

--- Northcoders/backend/drinksapi/src/main/java/com/northcoders/drinksapi/controller/HomeController.java
+++ Northcoders/backend/drinksapi/src/main/java/com/northcoders/drinksapi/controller/HomeController.java
@@ -2,7 +2,10 @@ package com.northcoders.drinksapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController()
 @RequestMapping("/")
@@ -14,7 +17,16 @@ public class HomeController {
     }
 
     @GetMapping("/coffeelover")
-    public String coffee(){
+    public String coffeeLover(){
         return "I like coffee!";
+    }
+
+    @GetMapping("/coffee")
+    public HashMap<String,Object> coffee(@RequestParam(value="name",defaultValue = "latte") String name){
+        Integer id_count = 1;
+        HashMap<String,Object> coffeeLog = new HashMap<>();
+        coffeeLog.put("id", id_count++);
+        coffeeLog.put("name", name);
+        return coffeeLog;
     }
 }
